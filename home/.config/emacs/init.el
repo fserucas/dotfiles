37ac2a24b20a2888f3c11abd8b782e6b6a61ac01
fserucas <97905656+fserucas@users.noreply.github.com>
2025-11-26 16:33:31 +0000

    add marginalia, minibuffer enricher

--- home/.config/emacs/init.el
+++ home/.config/emacs/init.el
@@ -70,6 +70,22 @@
 :ensure t)
 (use-package transpose-frame
 :ensure t)
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+	     :ensure t
+	     ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+	     ;; available in the *Completions* buffer, add it to the
+	     ;; `completion-list-mode-map'.
+	     :bind (:map minibuffer-local-map
+			 ("M-A" . marginalia-cycle))
+
+	     ;; The :init section is always executed.
+	     :init
+
+	     ;; Marginalia must be activated in the :init section of use-package such that
+	     ;; the mode gets enabled right away. Note that this forces loading the
+	     ;; package.
+	     (marginalia-mode))
 
 ;; Set Emacs Warning to error level, disable if you wnat higher level of warnings
 (setq warning-minimum-level :error)
